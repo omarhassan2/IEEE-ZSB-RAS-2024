a15e7d30859394510a19a4b9c933ebb4b102bfba
Omar Hassan <oh458886@gmail.com>
2024-04-06 14:43:33 +0200

    Submitted-Task-2

--- Task_2/Assignment_1/program_2.c
+++ Task_2/Assignment_1/program_2.c
@@ -20,8 +20,16 @@ int main(){
     int var = 0x332211;
     char *ptr = &var;
 
-    printf("*ptr++ = 0x%X\n", *ptr++); /* ptr -> 0x11, print = 0x11, ptr -> 0x22*/
-    printf("*++ptr = 0x%X\n", *++ptr); /* ptr -> 0x33, print = 0x33, ptr -> 0x33*/
-    printf("++*ptr = 0x%X\n", ++*ptr); /* ptr -> 0x33, print 0x33 + 0x01 = 0x34, ptr -> 0x34 */
+    /* it will get the data then the pointer move one step */
+    /* ptr -> 0x11, print = 0x11, ptr -> 0x22*/
+    printf("*ptr++ = 0x%X\n", *ptr++); 
+
+    /* The pointer will move one step then get the data */
+    /* ptr -> 0x33, print = 0x33, ptr -> 0x33*/
+    printf("*++ptr = 0x%X\n", *++ptr); 
+
+    /* it will get the data and add 0x01 to it, the pointer will not move */
+    /* ptr -> 0x33, print 0x33 + 0x01 = 0x34, ptr -> 0x34 */
+    printf("++*ptr = 0x%X\n", ++*ptr); 
 }
 /*********** Main function End ***********/
